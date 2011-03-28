2889b9d0137af968bf3c5ddc8b88dd2c6b87a6c0
Prashant-Sahni <prashant.sahni5@gmail.com>
2011-03-28 20:24:04 +0530

    Initial Commit: [Basic code added for playing video (Video for everyone)]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VideoPlayer::Application.config.secret_token = '7799da4c3a07f2c966f931d9f22f56c28fdf552e0b88ef875c791bf0e8b94b2f6bfec8fc74d87a4aaeb6715349f3b3d174421d29840a2f2502f57ba03665e128'
